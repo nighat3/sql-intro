c47aa2337b1361dd56421595b939ef33b1ae4396
Nighat Ansari <nighat.ansari@kellogg.northwestern.edu>
2025-01-16 03:08:47 +0000

    week 2 lab 4-1 and 4-2

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,9 @@
 -- How many lifetime hits does Barry Bonds have?
 
+select sum(stats.hits) from stats
+inner join players on players.id = stats.player_id
+where players.first_name = 'Barry' and players.last_name = 'Bonds'
+
 -- Expected result:
 -- 2935
 
--- lab-4-2.sql
+++ lab-4-2.sql
@@ -5,6 +5,14 @@
 -- WHERE column1 = expression1
 -- AND column2 = expression2
 
+select players.first_name, players.last_name
+from stats 
+inner join players on players.id = stats.player_id
+inner join teams on teams.id = stats.team_id
+where teams.name = 'Chicago Cubs'
+and teams.year = '2020'
+
+
 -- Expected result: 47 rows starting with
 --
 -- +------------+-----------+
