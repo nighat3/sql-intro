f4340a009f8dee9d4caa02b7e43d0e8bf7074130
Nighat Ansari <nighat.ansari@kellogg.northwestern.edu>
2025-01-18 12:38:11 +0000

    lab 4 3

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,12 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
 
+select teams.name as team_name, players.first_name as first_name, players.last_name as last_name, stats.home_runs as home_runs
+from stats 
+inner join players on players.id = stats.player_id
+inner join teams on teams.id = stats.team_id
+where teams.year = 2019
+order by home_runs desc
+limit 1
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
